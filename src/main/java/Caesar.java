920f923aade3fe80a01eaa0a12ac2e42f3a33b62
natewong <nwwong17@gmail.com>
2021-09-18 16:53:07 -0700

    Created caesar cipher encryptor and decryptor for APCS lab

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -5,35 +5,119 @@ public class Caesar {
     // and returns the encrypted String
     // Ex. encryptCaesar("Hello, World!") => "Khoor, Zruog!"
     public static String encryptCaesar(String message) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
+        String ans = "";
+        for(int i=0; i<message.length(); i++)
+        {
+            char letter = message.charAt(i); //the original letter from message
+            int letteri = letter; //int tracking which chars to use
+            if((letter>='A'&&letter<='Z')||(letter>='a'&&letter<='z')) //is the char in between A-Z or a-z and shift 3
+            {
+                letteri+=3;
+                char newchar = (char) letteri; //the revised char
+                if((letteri>='['&&letteri<=']')||(letteri>='{')) // if char is shifted 3 too far in the symbols, subtract 26 to loop around
+                {
+                    letteri-=26;
+                    newchar = (char) letteri;
+                }
+                ans+=newchar;//add the character to full answer
+            }
+            else{
+                ans+=letter;
+            }
+        }
+        return ans;
     }
 
     // Given a String, decrypts the String with the Caesar cipher
     // and returns the original String
     // Ex. decryptCaesar("Khoor, Zruog!") => "Hello, World!"
     public static String decryptCaesar(String message) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
+        String ans = "";
+        for(int i=0; i<message.length(); i++)
+        {
+            char letter = message.charAt(i); //the original letter from message
+            int letteri = letter; //int tracking which chars to use
+            if((letter>='A'&&letter<='Z')||(letter>='a'&&letter<='z')) //is the char in between A-Z or a-z and shift 3
+            {
+                letteri-=3;
+                char newchar = (char) letteri; //the revised char
+                if((letteri<='@')||(letteri>='^'&&letteri<='`')) // if char is shifted 3 too far in the symbols, add 26 to loop around
+                {
+                    letteri+=26;
+                    newchar = (char) letteri;
+                }
+                ans+=newchar;//add the character to full answer
+            }
+            else{
+                ans+=letter;
+            }
+        }
+        return ans;
     }
 
     // Given a String and a key corresponding to a shift, encrypts
     // the String using the given key and returns the encrypted String
     // Ex. encryptCaesarKey("Hello, World!", 5) => "Mjqqt, Btwqi!".
     public static String encryptCaesarKey(String message, int key) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
+        String ans = "";
+        for(int i=0; i<message.length(); i++)
+        {
+            char letter = message.charAt(i); //the original letter from message
+            int letteri = letter; //int tracking which chars to use
+            if((letter>='A'&&letter<='Z')) //is it an uppercase char?
+            {
+                letteri+=key;
+                while(letteri>'Z') // accounts for chars that go over the range of uppercase letters, and keeps subtracting 26 until it is in range
+                    {
+                        letteri-=26;
+                    }
+            }
+            else if((letter>='a'&&letter<='z')) //is it a lowercase char?
+            {
+                letteri+=key;
+                while(letteri>'z') // accounts for chars that go over that go over the range of lowercase letters, and keeps subtracting 26 until it is in range
+                    {
+                        letteri-=26;
+                    }
+            }
+            char finalchar = (char) letteri;
+            ans+=finalchar;
+        }
+        return ans;
     }
 
     // Given a String and a key corresponding to a shift, decrypts
     // the String using the given key and returns the original String
     // Ex. decryptCaesarKey("Mjqqt, Btwqi!", 5) => "Hello, World!"
     public static String decryptCaesarKey(String message, int key) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
+        String ans = "";
+        for(int i=0; i<message.length(); i++)
+        {
+            char letter = message.charAt(i); //the original letter from message
+            int letteri = letter; //int tracking which chars to use
+            if((letter>='A'&&letter<='Z')) //is it an uppercase char?
+            {
+                letteri-=key;
+                while(letteri<'A') //accounts for chars that go under that go over the range of uppercase letters, and keeps adding 26 until it is in range
+                    {
+                        letteri+=26;
+                    }
+
+            }
+            else if((letter>='a'&&letter<='z')) //is it a lowercase char?
+            {
+                letteri-=key;
+                while(letteri<'a') //accounts for chars that go under that go over the range of lowercase letters, and keeps adding 26 until it is in range
+                    {
+                        letteri+=26;
+                    }
+
+            }
+            char finalchar = (char) letteri;
+            ans+=finalchar;
+        }
+        return ans;
     }
-
-
     // The main method is already provided for you
     // You do not need to edit this code until Part 2
     public static void main(String[] args) {
@@ -57,7 +141,6 @@ public class Caesar {
         else {
             System.out.println("Unknown command; please type either \"encrypt\" or \"decrypt\"");
         }
-
         scan.close();
     }
 }
