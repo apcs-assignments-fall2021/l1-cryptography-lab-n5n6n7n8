b7f37ba117a6bb894a631d978d63537f1aae2521
natewong <nwwong17@gmail.com>
2021-09-20 12:33:32 -0700

    Added vingenere cipher option for caesar cipher encryptor and decryptor for APCS lab

--- src/main/java/Vigenere.java
+++ src/main/java/Vigenere.java
@@ -2,13 +2,73 @@ import java.util.Scanner;
 
 public class Vigenere {
     public static String encryptVigenere(String message, String key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String ans = "";
+        int i2 = 0;//second i to increment through the letters in key
+        for(int i=0; i<message.length(); i++)
+        {
+            char letter = message.charAt(i); //the original char from message
+            int letteri = letter; //int tracking which chars to use
+            if(letter>='A'&&letter<='Z') //is it uppercase?
+            {
+                letteri+=convertLetter(key.charAt(i2));//add the int of the key to the output char
+                if(letteri>'Z') // accounts for chars that go over the range of uppercase letters, and subtracts 26 so it is in range
+                {
+                    letteri-=26;
+                }
+                i2++;
+            }
+            else if(letter>='a'&&letter<='z')//is it lowercase?
+            {
+                letteri+=convertLetter(key.charAt(i2));//add the int of the key to the output char
+                if(letteri>'z') // accounts for chars that go over the range of lowercase letters, and subtracts 26 so it is in range
+                {
+                    letteri-=26;
+                }
+                i2++;
+            }
+            char finalchar = (char) letteri;
+            ans+=finalchar;
+            if(i2>=key.length()) //increment 2 resets to zero after key reaches the length
+            {
+                i2=0;
+            }
+        }
+        return ans;
     }
 
     public static String decryptVigenere(String message, String key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String ans = "";
+        int i2 = 0;//second i to increment through the letters in key
+        for(int i=0; i<message.length(); i++)
+        {
+            char letter = message.charAt(i); //the original char from message
+            int letteri = letter; //int tracking which chars to use
+            if(letter>='A'&&letter<='Z') //is it uppercase?
+            {
+                letteri-=convertLetter(key.charAt(i2));//subtract the int of the key to the output char
+                if(letteri<'A') // accounts for chars that go under the range of uppercase letters, and adds 26 so it is in range
+                {
+                    letteri+=26;
+                }
+                i2++;
+            }
+            else if(letter>='a'&&letter<='z')//is it lowercase?
+            {
+                letteri-=convertLetter(key.charAt(i2));//add the int of the key to the output char
+                if(letteri<'a') // accounts for chars that go under the range of lowercase letters, and adds 26 so it is in range
+                {
+                    letteri+=26;
+                }
+                i2++;
+            }
+            char finalchar = (char) letteri;
+            ans+=finalchar;
+            if(i2>=key.length()) //increment 2 resets to zero after key reaches the length
+            {
+                i2=0;
+            }
+        }
+        return ans;
     }
 
 
@@ -37,7 +97,10 @@ public class Vigenere {
         else {
             System.out.println("Unknown command; please type either \"encrypt\" or \"decrypt\"");
         }
-
         scan.close();
+    }
+    public static int convertLetter(char ch)
+    {
+        return ch - 'A';
     }
 }
